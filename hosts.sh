b9d47f1e6d00a2775528fb89d10681f0fd81da55
giooooo <gbperalta1@up.edu.ph>
2018-12-06 16:15:07 +0800

    Add workaround to run app in 64bit devices Change hosts script

--- hosts.sh
+++ hosts.sh
@@ -1,6 +1,8 @@
 rm hosts
 adb root
 adb remount
-adb pull /system/etc/hosts hosts
+adb pull /etc/hosts hosts
 echo "10.0.2.2\tswinecart.test\n" >> hosts
-adb push hosts /system/etcs/hosts
+adb push hosts /etc/hosts
+adb pull /etc/hosts hosts
+cat hosts
