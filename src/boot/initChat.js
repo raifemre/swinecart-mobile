1f43c8f87c7bf213a8bb070944eff0d824244d76
giooooo <gbperalta1@up.edu.ph>
2018-12-06 13:53:48 +0800

    Add reconnect for websocket connections

--- src/boot/initChat.js
+++ src/boot/initChat.js
@@ -5,41 +5,49 @@ import {
 import UserStore from '../mobx/stores/UserStore';
 import MessageStore from '../mobx/stores/MessageStore';
 
+const connect = () => {
+  const ws = new WebSocket('ws://swinecart.test/chat');
+
+  ws.onopen = () => {
+    console.log('WebSocket:', 'Connected!');
+    
+    const message = {
+      from: UserStore.userId,
+      to: null,
+      message: 'Connection established.',
+      direction: null
+    };
+
+    MessageStore.setSocket(ws);
+    ws.send(JSON.stringify(message));
+  }
+
+  ws.onmessage = ({ data }) => {
+    const message = JSON.parse(data);
+    const { to } = message;
+    if (UserStore.userId === parseInt(to)) {
+      MessageStore.handleReceivedMessage(message);
+    }
+
+  }
+
+  ws.onerror = (e) => {
+    console.log('WebSocket:', e.message);
+  };
+
+  ws.onclose = (e) => {
+    if (UserStore.userId) {
+      setTimeout(() => {
+        console.log('WebSocket:', 'Reconnecting!...');
+        connect();
+      }, 1000);
+    }
+  };
+}
+
 export default function()  {
-  const userId = UserStore.userId;
 
-  if(userId) {
-    const ws = new WebSocket('ws://swinecart.test/chat');
-
-    ws.onopen = () => {
-      console.log('WebSocket:', 'Opened!');
-
-      const message = {
-        from: userId,
-        to: null,
-        message: 'Connection established.',
-        direction: null
-      };
-
-      MessageStore.setSocket(ws);
-      ws.send(JSON.stringify(message));
-    }
-
-    ws.onmessage = ({ data }) => {
-      const message = JSON.parse(data);
-      const { to } = message;
-      if(userId === parseInt(to)) {
-        MessageStore.handleReceivedMessage(message);
-      }
-
-    }
-
-    ws.onerror = e => {
-      console.log('WebSocket:', e.message);
-    }
-
-    ws.onclose = () => {
-      console.log('WebSocket:', 'Closed');
-    }
+  if(UserStore.userId) {
+    connect();
   }
 }
