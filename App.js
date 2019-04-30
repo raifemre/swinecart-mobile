72eb8bf8457f260f61ed06c025c31c5e9c223657
giooooo <gbperalta1@up.edu.ph>
2019-04-30 12:58:20 +0800

    Upgrade RN version

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+export default App;
