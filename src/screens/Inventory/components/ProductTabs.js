9310c29101d29766850eaa953e2505a729c5bbb0
giooooo <gbperalta1@up.edu.ph>
2019-05-09 16:22:12 +0800

    UI Revamp of Inventory

--- src/screens/Inventory/components/ProductTabs.js
+++ src/screens/Inventory/components/ProductTabs.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { Dimensions } from 'react-native';
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 
 import RequestedProducts from './RequestedProducts';
 import ReservedProducts from './ReservedProducts';
+import OnDeliveryProducts from './OnDeliveryProducts';
+import SoldProducts from './SoldProducts';
 
 class ProductsTabs extends Component {
 
@@ -19,23 +22,28 @@ class ProductsTabs extends Component {
   renderTabBar = props => (
     <TabBar
       {...props}
+      scrollEnabled
+      style={{ backgroundColor: '#00695C' }}
+      indicatorStyle={{ backgroundColor: '#ffffff' }}
+      labelStyle={{ fontFamily: 'Rubik-Bold', }}
+      initialLayout={{ width: Dimensions.get('window').width }}
     />
   )
 
   render() {
     return (
       <TabView
-        scrollEnabled  
         navigationState={this.state}
         renderScene={SceneMap({
           '1': RequestedProducts,
           '2': ReservedProducts,
-          '3': RequestedProducts,
-          '4': RequestedProducts,
+          '3': OnDeliveryProducts,
+          '4': SoldProducts,
         })}
+
         onIndexChange={index => this.setState({ index })}
         renderTabBar={this.renderTabBar}
-        lazy={false}
+        lazy={true}
       />
     );
   }
