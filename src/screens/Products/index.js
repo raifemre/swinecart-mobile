ad39d8f67325c3e950918eeee24842d33436d493
giooooo <gbperalta1@up.edu.ph>
2019-09-19 03:43:40 +0800

    refactor: remove not needed imports

--- src/screens/Products/index.js
+++ src/screens/Products/index.js
@@ -1,18 +1,10 @@
-import React, { Fragment, memo, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { Fragment, memo } from 'react';
 
-import {  HeaderBar, AddProductButton } from '../../shared/components';
+import {  HeaderBar, AddProductButton } from 'shared/components';
+
 import { ProductsList } from './components'
 
-import { fetchProducts } from '../../redux/actions/products';
-
 function Container() {
-  
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, []);
 
   return (
     <Fragment>
